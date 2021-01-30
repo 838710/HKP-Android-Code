b0a9263cac865e070a5355467e685b62a06002b2
unknown <838710@lcps.org>
2021-01-29 22:36:06 -0500

    First commit

---
+++ Solution.java
@@ -0,0 +1,18 @@
+/*public class Solution {
+    public static void main (String[] args){
+        System.out.println("Hello world!");
+    }
+}*/
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] x = {1, 1};
+        return x;
+    }
+    public static void main (String[] args){
+        int[] thing = {0, 1};
+        System.out.println(Arrays.toString(searchRange(thing, 1)));
+    }
+}
+
